49a27d01af8e38634292ecff3e6bdf0ac860f150
CodeinHub <codeinhub@gmail.com>
2020-02-17 21:50:52 -0500

    new branch

--- ReviewGitProject/src/GitReviewClass.java
+++ ReviewGitProject/src/GitReviewClass.java
@@ -7,6 +7,7 @@ public class GitReviewClass {
 		System.out.println("Review Day - locally");
 		System.out.println("Review Day - locally2");
 		System.out.println("Review Day - remote");
+		System.out.println("Review Day - locally3");
 	}
 
 }
